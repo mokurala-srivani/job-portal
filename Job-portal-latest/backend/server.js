7b4db1b37b9491e949580be486c865f636ec5ad4
mokurala srivani <mokuralasrivani55@gmail.com>
2026-05-08 09:44:16 +0530

    fixed cors

--- Job-portal-latest/backend/server.js
+++ Job-portal-latest/backend/server.js
@@ -8,7 +8,13 @@ const { notFound, errorHandler } = require('./middleware/error');
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || '*', credentials: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://job-portal-alpha-amber.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV !== 'test') app.use(morgan('dev'));
